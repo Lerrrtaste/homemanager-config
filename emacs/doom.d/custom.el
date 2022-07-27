22faef63f76629e2276c6ccef600b1ef47c0ce37
Lerrrtaste <lerrrtaste@protonmail.com>
2022-07-27 23:16:43 +0200

    [emacs] set up config.org and add first real configurations

--- emacs/doom.d/custom.el
+++ emacs/doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default)))
+   '("745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
