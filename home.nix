f4e4880122692c76a2a3fec003ca92e22901e9f8
Laurenz Foglia <lerrrtaste@protonmail.com>
2022-10-05 13:57:31 +0200

    Disable latex

--- home.nix
+++ home.nix
@@ -113,7 +113,7 @@ in
     libreoffice
     zathura
     firefox # TODO keep as seperate package (ff from module with different name, then add keybinds (shift)-alt-b to open them)
-    texlive.combined.scheme-full  # latex full (ca 5 gb!)
+    # texlive.combined.scheme-full  # latex full (ca 5 gb!)
     teams
     # protonmail-bridge # TODO write emacs mu4e config
 
