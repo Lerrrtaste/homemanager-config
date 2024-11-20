00ae08639f8ff5afb084eeb01c2daff46eac92f9
Laurenz Foglia <lerrrtaste@protonmail.com>
2024-11-20 03:50:12 +0100

    Refactor emacs init module; Update to 28.x

--- emacs/init.nix
+++ emacs/init.nix
@@ -1,43 +1,5 @@
 { config, pkgs, lib, ... }:
 
-# {
-#   # nixpkgs.overlays = [ (self: super: { nodejs = self.nodejs-17_x; }) ];
-#   services.emacs.enable = true;
-
-#   programs.emacs = {
-#     enable = true;
-#   };
-
-#   home.packages = with pkgs; [
-#     # required deps (doom)
-#     git
-#     ripgrep
-
-#     # optionsl deps (doom)
-#     fd
-#     clang
-#     pinentry_emacs
-#     zstd
-#     sqlite
-
-#     # copilot
-#     nodejs-16_x
-
-#     # sh
-#     shellcheck
-
-#     # web
-#     html-tidy
-
-#     # nix
-#     nixfmt
-
-#   # cc
-#   libclang
-
-
-#   ];
-
 #   # FIXME it always detects changes when there are non
 #   # maybe link the files so i can reload by hand too someow
 #   home.file.".doom.d" = {
@@ -51,12 +13,16 @@ let
   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
     url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
   }) {
-    doomPrivateDir = ./doom.d;  # Directory containing your config.el, init.el
+    doomPrivateDir = /home/lerrrtaste/.doom.d;  # Directory containing your config.el, init.el
                                 # and packages.el files
   };
 in {
+nixpkgs.overlays = [ # required for doom according to docs
+  (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
+];
   home.packages = [
-    # doom-emacs #FIXME maybe...
+    doom-emacs #FIXME maybe...
+    # pkgs.emacsGcc  # Installs Emacs 28 + native-comp
 
     # required deps (doom)
     pkgs.git
@@ -92,9 +58,13 @@ in {
   ];
 
   services.emacs = {
-    enable = true;
+    enable = false; # broken
+    package = pkgs.emacsGcc;
   };
-  programs.emacs.enable = true;
+  # programs.emacs = {
+  #   enable = true;
+  #   package = pkgs.emacs-git;
+  # };
 
    # FIXME it always detects changes when there are non
    # maybe link the files so i can reload by hand too someow
